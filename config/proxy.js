7427b89d55047870cd4f1ba93bff2ad128bf4b0d
Weismann <1563511027@qq.com>
2019-08-22 00:39:10 +0800

    增加 README.md

--- config/proxy.js
+++ config/proxy.js
@@ -2,11 +2,13 @@ const proxy = require('http-proxy-middleware');
 
 //接口地址配置
 const scoketConfig = {
+    // index: {
+
+    // },
     test: {
         host: 'http://localhost:3000',
         uri: '/api'
     },
-
     
 }
 
